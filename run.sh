e73ac8f475bb5c69a4e04c84345a6be41d6284b6
centron-sg1 <136540701+centron-sg1@users.noreply.github.com>
2026-07-15 14:05:43 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 export AWS_ACCESS_KEY_ID=$(jq -r '.aws_access_key_id' /data/options.json)
 export AWS_SECRET_ACCESS_KEY=$(jq -r '.aws_secret_access_key' /data/options.json)
 
-python3 /app.py
+python3 /app/app.py
